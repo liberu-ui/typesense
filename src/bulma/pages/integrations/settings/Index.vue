d1e325f635ad8cfb307b54d76d5bc0a2b7fb602d
mishu <mishu@xtelecom.ro>
2021-10-07 14:34:24 +0300

    cleanup form

--- src/bulma/pages/integrations/settings/Index.vue
+++ src/bulma/pages/integrations/settings/Index.vue
@@ -2,10 +2,7 @@
     <div class="columns is-centered">
         <div class="column is-half-desktop is-full-touch">
             <enso-form class="box has-background-light raises-on-hover"
-                :key="key"
-                @ready="form = $event.form"
-                @submit="submit"
-                ref="form"/>
+                @ready="form = $event.form"/>
         </div>
     </div>
 </template>
@@ -20,15 +17,6 @@ export default {
 
     data: () => ({
         form: null,
-        key: 1,
     }),
-
-    methods: {
-        submit() {
-            if (!this.form.routeParam('settings')) {
-                this.key++;
-            }
-        },
-    },
 };
 </script>
