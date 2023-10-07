876b5c73b43f99a7fccbe9cab230850dbd3b4be9
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:27:49 +0000

    Replace JavaScript with TypeScript

--- src/bulma/routes/integrations.js
+++
@@ -1,8 +0,0 @@
-import routeImporter from '@liberu-ui/ui/src/modules/importers/routeImporter';
-
-const routes = routeImporter(require.context('./integrations', false, /.*\.js$/));
-
-export default {
-    path: '/integrations',
-    children: routes,
-};
--- src/icons.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSearch, faPuzzlePiece, faUserCog} from '@fortawesome/free-solid-svg-icons';
-
-library.add(faSearch, faPuzzlePiece, faUserCog);
